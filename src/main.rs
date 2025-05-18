8d2e3d50d501a6d0efbb7a326c3ac5180ecf4ab3
notme <notme3029@gmail.com>
2025-05-18 11:07:14 +0200

    proper frame updates

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ndarray::prelude::*;
 use ndarray::Array;
 use colored::Colorize;
+use std::time::{Duration, Instant};
+use std::thread::sleep;
 
 //https://tetris.wiki/Tetris_Guideline
 //https://tetris.fandom.com/wiki/SRS?file=SRS-pieces.png
@@ -11,10 +13,11 @@ fn main() {
     let p = Piece::new(Block::Green);
     println!("{:?}", p.matrix);
 
-    /*let x = 10;
-    let y = 40;
-    let mut a = Array::<Block, _>::from_elem((y, x).f(), Block::Red);
+    let size_x = 10;
+    let size_y = 40;
+    let mut a = Array::<Block, _>::from_elem((size_y, size_x).f(), Block::Red);
     a[[0,5]] = Block::LightBlue;
+
 
     let mut idx = 0;
     for b in Block::VALUES{
@@ -22,19 +25,67 @@ fn main() {
         idx += 1;
     }
 
-    for (i, b) in a.iter().enumerate(){
-        b.print();
-        if (i+1) % 10 == 0{
-            println!("\n");
-        }
-    }*/
-    
-    //println!("{:?}", a);
 
-    //let b = Block::Green;
-    //println!("Green: {}", b as i32);
-    //println!("Red: {}", Block::Red as i32);
-    //println!("None: {}", Block::None as i32);
+
+
+    let frame_time = Duration::from_millis(250);
+    let mut last_frame = Instant::now();
+
+    loop {
+        
+        
+
+        let mut new_screen = Array::<Block, _>::from_elem((size_y, size_x).f(), Block::None);
+        //println!("created new_screen. x: {}, y: {}", size_x, size_y);
+
+        //i want to revert this but cant
+        for (i, b) in a.iter().enumerate(){
+            
+            let x = i % size_x;
+            let y = (i - x)/size_x;
+
+            //println!("x: {}, y: {}", x, y);
+
+
+            //new_screen[[y,x]] = Block::None;
+            if y+1 < size_y{
+                new_screen[[y+1, x]] = *b;
+            }
+        }
+
+        a = new_screen;
+
+
+        //println!("before clearing screen");
+        //this seemingly just fills up the screen with invisible chars, which is good enough i guess but i dont like it
+        print!("{}[2J", 27 as char);
+        //println!("after clearing screen");
+
+
+        //print all blocks
+        let mut out = String::from("");
+        for (i, b) in a.iter().enumerate(){
+            //b.print();
+            out += &b.get_string_rep();
+            if (i+1) % 10 == 0{
+                out += "\n";
+                //println!("\n");
+            }
+        }
+        println!("{}", out);
+
+
+        let time = Instant::now();
+        if let Some(sleep_for) = frame_time.checked_sub(time.duration_since(last_frame)){
+
+            //println!("sleep for is {} ms", sleep_for.as_millis());
+            if !sleep_for.is_zero() {
+                sleep(sleep_for);
+            }
+        }
+
+        last_frame = Instant::now();
+    }
 
     //https://github.com/rust-ndarray/ndarray/blob/master/README-quick-start.md
 }
@@ -56,23 +107,18 @@ impl Block{
     const VALUES: [Self; 8] = [Block::LightBlue, Block::DarkBlue, Block::Orange, Block::Yellow, Block::Green, Block::Red, Block::Magenta, Block::None];
 }
 
+impl std::fmt::Display for Block{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
 #[derive(Copy, Clone)]
 pub struct Color(u8, u8, u8);
 
 
 impl Block{
     fn print(&self){
-
-        /*
-        print!("{}",
-        
-        match self{
-            Block::LightBlue => Colorize::bright_blue("[]"),
-            Block::DarkBlue => Colorize::blue("[]"),
-            Block::Orange => Colorize::truecolor("[]", 128, 128, 0),
-
-            _ => Colorize::black("  ")
-        });*/
 
         let colors = [
             Color(50,50,180),
@@ -89,6 +135,21 @@ impl Block{
         print!("{}", Colorize::truecolor("[]", c.0, c.1, c.2));
     }
 
+    fn get_string_rep(&self) -> String{
+        let colors = [
+            Color(50,50,180),
+            Color(0,0,255),
+            Color(128, 30, 0),
+            Color(128, 128, 0),
+            Color(0,255,0),
+            Color(255, 0, 0),
+            Color(128, 0, 128),
+            Color(0,0,0)
+        ];
+
+        let c = colors[*self as usize];
+        format!("{}", Colorize::truecolor("[]", c.0, c.1, c.2))
+    }
 }
 
 
